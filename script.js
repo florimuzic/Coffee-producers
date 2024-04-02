1fef0eaaca3adf7a937293632843d3cae35113ce
florimuzic <149759685+florimuzic@users.noreply.github.com>
2024-04-02 11:32:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let brazilTag = document.querySelector(".brazil-tag")
+let brazilImage = document.querySelector(".brazil-img")
+
+brazilTag.addEventListener("mouseover", function(){
+   brazilImage.classList.add("zindex");
+})
+
+brazilTag.addEventListener("mouseout", function(){
+   brazilImage.classList.remove("zindex");
+})
+
+let vietnamTag = document.querySelector(".vietnam-tag")
+let vietnamImage = document.querySelector(".vietnam-img")
+
+vietnamTag.addEventListener("mouseover", function(){
+   vietnamImage.classList.add("zindex");
+})
+
+vietnamTag.addEventListener("mouseout", function(){
+   vietnamImage.classList.remove("zindex");
+})
+
+let colombiaTag = document.querySelector(".colombia-tag")
+let colombiaImage = document.querySelector(".colombia-img")
+
+colombiaTag.addEventListener("mouseover", function(){
+   colombiaImage.classList.add("zindex");
+})
+
+colombiaTag.addEventListener("mouseout", function(){
+   colombiaImage.classList.remove("zindex");
+})
+
+let indonesiaTag = document.querySelector(".indonesia-tag")
+let indonesiaImage = document.querySelector(".indonesia-img")
+
+indonesiaTag.addEventListener("mouseover", function(){
+   indonesiaImage.classList.add("zindex");
+})
+
+indonesiaTag.addEventListener("mouseout", function(){
+   indonesiaImage.classList.remove("zindex");
+})
+
+let ethiopiaTag = document.querySelector(".ethiopia-tag")
+let ethiopiaImage = document.querySelector(".ethiopia-img")
+
+ethiopiaTag.addEventListener("mouseover", function(){
+   ethiopiaImage.classList.add("zindex");
+})
+
+ethiopiaTag.addEventListener("mouseout", function(){
+   ethiopiaImage.classList.remove("zindex");
+})
